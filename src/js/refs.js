9a124583afeef967079f3f94ed724dd88c5a71a0
Suizanna <73129581+Suizanna@users.noreply.github.com>
2021-05-23 07:21:21 +0300

    add

---
+++ src/js/refs.js
@@ -0,0 +1,5 @@
+export default {
+    formRef: document.querySelector('.search-form'),
+    galleryRef: document.querySelector('.gallery'),
+    loadMoreBtn: document.querySelector('.load-more-button'),
+};
---
+++ src/js/search.js
@@ -0,0 +1,3 @@
+import imageCardTpl from '../templates/image-card.hbs';
+import ApiService from './apiService.js';
+import refs from './refs';
