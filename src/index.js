8daee1fc83acaad2bc9be38e43f2a0e9e3d0a8ca
Suizanna <73129581+Suizanna@users.noreply.github.com>
2021-05-27 00:44:47 +0300

    add

--- src/index.js
+++ src/index.js
@@ -1,54 +1,7 @@
 import './styles.css';
 import './js/search.js';
 
-// import picturesTpl from './templates/image-card.hbs';
-// import fetchPicture from './js/apiService';
-// import * as basicLightbox from 'basiclightbox';
-// import './basicLightbox.min.css';
 
-// const formSearchRef = document.querySelector('#search-form');
-// const picturesGalery = document.querySelector('.gallery');
-// const buttonRef = document.querySelector('.load-more');
-// const inputRef = document.querySelector('input');
-
-
-// let pageNumber = 1;
-
-// formSearchRef.addEventListener('submit', event => {
-    
-//     event.preventDefault();
-//     pageNumber = 1;
-//     picturesGalery.innerHTML = '';
-//     fetchPicture(pageNumber, inputRef.value).then(renderPictures);
-// });
-
-// function renderPictures(data) {
-    
-//     picturesGalery.insertAdjacentHTML('beforeend', picturesTpl(data));
-//     window.scrollTo(
-//         {
-//             top: scrollHeight,
-//             left: 0,
-//             behavior: 'smooth',
-//         });
-// }
-
-// buttonRef.addEventListener('click', () => {
-//     pageNumber += 1;
-//     fetchPicture(pageNumber, inputRef.value).then(renderPictures);
-    
-// })
-
-// picturesGalery.addEventListener('click', (event) => {
-//     if (event.target.nodeName !== 'IMG') {
-//         return;
-//     }
-
-//     basicLightbox.create(`
-//     <img src=${event.target.dataset.source} width="800" height="600">
-// `).show()
-
-// })
 
 
 
