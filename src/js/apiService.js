f146325f770d586e409de2b3d343b9c56483bea0
Suizanna <73129581+Suizanna@users.noreply.github.com>
2021-05-28 00:40:47 +0300

    add

--- src/js/apiService.js
+++ src/js/apiService.js
@@ -2,8 +2,14 @@ import axios from "axios";
 
 axios.defaults.baseURL = 'https://pixabay.com/api/';
 
+const BASE_URL  = `https://pixabay.com/api/?image_type=photo&orientation=horizontal`;
+const  KEY = `&key=15900106-2c235e732bb321ca7ec900d93`;
+const perPage = `&per_page=12`;
+
+
+
 export default function fetchPictures(page, query) {
-    return axios('?image_type=photo&orientation=horizontal&q=' + query + `&page=${page}&per_page=12&key=6482848-443bdcc87ab7c743d3374b0c5`).then(response => response.data.hits);
+ return axios.get(`${BASE_URL}&q=${query}&page=${page}&per_page=${perPage}&key=${KEY}` ).then(response => response.data.hits);
 
 }
 
@@ -11,4 +17,3 @@ export default function fetchPictures(page, query) {
 
 
 
- 
--- src/js/search.js
+++ src/js/search.js
@@ -1,8 +1,9 @@
-
 import picturesTpl from '../templates/image-card.hbs';
 import fetchPictures from './apiService.js';
+//плагин модального окна. показ большой картинки 
 import * as basicLightbox from 'basiclightbox';
 import '../basicLightbox.min.css';
+import errorNotification from './pnotify.js';
 
 // console.log(fetchPictures('cat'));
 
@@ -13,31 +14,47 @@ const inputRef = document.querySelector('input');
 
 let pageNumber = 1;
 
-
-formRef.addEventListener('submit', event => {
+formRef.addEventListener('submit', onSearch);
+  
+function onSearch(event) {
     event.preventDefault();
     //начинаем новый запрос с 1 стр
      pageNumber = 1; 
     //очищаем при новом запросе
     galleryRef.innerHTML = '';
     //получаем картинки
-    fetchPictures(pageNumber, inputRef.value).then(renderPictures);//наш fetch
-});
+    fetchPictures(pageNumber, inputRef.value).then(renderPictures);
+}
 
 //рендерим картинки
 function renderPictures(data) { 
+    //если не некорректный запрос и картинки не пришли
+    if (data.length === 0) {
+        errorNotification('No matches found. Try again.');
+      }
+      //картинки
     galleryRef.insertAdjacentHTML('beforeend', picturesTpl(data));
-    window.scrollTo(
+    //кнопку load-more
+    if (data.length < 12) {
+        buttonRef.classList.add('is-hidden');
+      } else {
+        buttonRef.classList.remove('is-hidden');
+      }
+    
+    window.scrollTo(  //не работает
         {
             top: scrollHeight,
             left: 0,
             behavior: 'smooth',
-        });
+        });          
+        
 }
 
 //увечиваем на 1 стр
 buttonRef.addEventListener('click', () => {
-    pageNumber += 1;
+    if(  pageNumber > 1 ) {
+      pageNumber += 1;
+    }
     //наши картинки
     fetchPictures(pageNumber, inputRef.value).then(renderPictures);
 });
@@ -47,8 +64,12 @@ galleryRef.addEventListener('click', (event) => {
     if (event.target.nodeName !== 'IMG') {
         return;
     }
-    basicLightbox.create(`
+    basicLightbox  //объект
+    .create(`
     <img src=${event.target.dataset.source} width="800" height="600">
 `).show()
 
 })
+
+
+
